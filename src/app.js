cf8b7daf095e3a0f4a7360b0b272123b8482ca3d
Muhammad Arslan <imarslan7061@gmail.com>
2025-03-12 06:19:49 +0500

    Future Proof Project Setup

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import e from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+const app = e()
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+
+}))
+
+app.use(e.json({limit: "16kb"}))
+
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 dotenv.config({
     path: "./env"
 })
 
+const PORT = process.env.PORT || 4000
 
 connectDB()
+.then(() => {
+    app.on("error", (err) => {
+        console.log("Error", err)
+        throw err
+    })
+    app.listen(PORT, () => {
+        console.log(`Server is Running on PORT: ${PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MongoDB Connection Failed !!", err)
+})
