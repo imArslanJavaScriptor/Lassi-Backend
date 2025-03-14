c83a4e8c1c227bc44280e00811e15ba2c4cac037
Muhammad Arslan <imarslan7061@gmail.com>
2025-03-14 10:05:34 +0500

    update working Setup

---
+++ src/models/video.models.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema(
+    {
+        videoFile: {
+            type: String,
+            required: true,
+        },
+        thumbnail: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true,
+            trim:true,
+        },
+        description: {
+            type: String,
+            required: true,
+            trim:true,
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+
+)
+
+export const Video = mongoose.model("Video", videoSchema)
